f68ccf211596b4e0031b8c25dee5a7e1171521eb
Jane Guo <j.guo@seismic.com>
2025-12-17 09:36:15 +0800

    Update draggable_flowchart_screen.dart

--- lib/ui/screens/draggable_flowchart_screen.dart
+++ lib/ui/screens/draggable_flowchart_screen.dart
@@ -1,4 +1,17 @@
-// New: flatten tree and insert subworkflow nodes from cache for visualization
+
+import 'package:flutter/material.dart';
+import '../../models/entities/workflow_node.dart';
+
+// --- Graph node and flattening logic ---
+class _GraphNode {
+  final WorkflowNode node;
+  final WorkflowNode? parent;
+  final int depth;
+  final bool isSubworkflow;
+  _GraphNode(this.node, this.parent, this.depth, {this.isSubworkflow = false});
+}
+
+// Flattens the workflow tree and inserts subworkflow nodes from cache for visualization
 List<_GraphNode> _flattenWithCache(List<WorkflowNode> roots, Map<String, WorkflowNode> cache) {
   final List<_GraphNode> result = [];
   void visit(WorkflowNode node, WorkflowNode? parent, int depth, {bool isSubworkflow = false}) {
@@ -20,52 +33,11 @@ List<_GraphNode> _flattenWithCache(List<WorkflowNode> roots, Map<String, Workflo
   }
   return result;
 }
-import 'package:flutter/material.dart';
-import '../../models/entities/workflow_node.dart';
 
-class _GraphNode {
-  final WorkflowNode node;
-  final WorkflowNode? parent;
-  final int depth;
-  final bool isSubworkflow;
-  _GraphNode(this.node, this.parent, this.depth, {this.isSubworkflow = false});
-
-// Utility to flatten the workflow tree and subworkflows into a list of nodes with parent info
-List<_GraphNode> flattenWorkflowTree(List<WorkflowNode> roots) {
-  final List<_GraphNode> result = [];
-  void visit(WorkflowNode node, WorkflowNode? parent, int depth, {bool isSubworkflow = false}) {
-    result.add(_GraphNode(node, parent, depth, isSubworkflow: isSubworkflow));
-    if (node.children.isNotEmpty) {
-      for (int i = 0; i < node.children.length; i++) {
-        visit(node.children[i], node, depth + 1, isSubworkflow: true);
-      }
-    }
-  }
-  for (final root in roots) {
-    visit(root, null, 0);
-  }
-  return result;
-}
-}
-
-// Utility to flatten the workflow tree into a list of nodes with parent info
-List<_GraphNode> flattenWorkflowTree(List<WorkflowNode> roots) {
-  final List<_GraphNode> result = [];
-  void visit(WorkflowNode node, WorkflowNode? parent, int depth) {
-    result.add(_GraphNode(node, parent, depth));
-    for (final child in node.children) {
-      visit(child, node, depth + 1);
-    }
-  }
-  for (final root in roots) {
-    visit(root, null, 0);
-  }
-  return result;
-}
-
-// ...existing code...
+class DraggableFlowchartScreen extends StatelessWidget {
   final List<WorkflowNode> nodes;
   final Map<String, WorkflowNode>? subworkflowCache;
+
   const DraggableFlowchartScreen({Key? key, required this.nodes, this.subworkflowCache}) : super(key: key);
 
   static const double nodeWidth = 180;
@@ -75,31 +47,7 @@ List<_GraphNode> flattenWorkflowTree(List<WorkflowNode> roots) {
 
   @override
   Widget build(BuildContext context) {
-    // Merge subworkflow cache into a visualization tree for display
     final flatNodes = _flattenWithCache(nodes, subworkflowCache ?? {});
-// New: flatten tree and insert subworkflow nodes from cache for visualization
-List<_GraphNode> _flattenWithCache(List<WorkflowNode> roots, Map<String, WorkflowNode> cache) {
-  final List<_GraphNode> result = [];
-  void visit(WorkflowNode node, WorkflowNode? parent, int depth, {bool isSubworkflow = false}) {
-    result.add(_GraphNode(node, parent, depth, isSubworkflow: isSubworkflow));
-    // If a subworkflow is cached for this node, use it for visualization
-    if (cache.containsKey(node.id)) {
-      final subNode = cache[node.id]!;
-      for (int i = 0; i < subNode.children.length; i++) {
-        visit(subNode.children[i], node, depth + 1, isSubworkflow: true);
-      }
-    } else if (node.children.isNotEmpty) {
-      for (int i = 0; i < node.children.length; i++) {
-        visit(node.children[i], node, depth + 1, isSubworkflow: true);
-      }
-    }
-  }
-  for (final root in roots) {
-    visit(root, null, 0);
-  }
-  return result;
-}
-
     return Scaffold(
       appBar: AppBar(title: const Text('Draggable Flowchart')),
       body: LayoutBuilder(
@@ -132,6 +80,28 @@ List<_GraphNode> _flattenWithCache(List<WorkflowNode> roots, Map<String, Workflo
     );
   }
 }
+
+class _FlowchartNode extends StatelessWidget {
+  final String label;
+  final bool isSubworkflow;
+  const _FlowchartNode({required this.label, this.isSubworkflow = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      color: isSubworkflow ? Colors.orange[100] : Colors.blue[100],
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(label, textAlign: TextAlign.center),
+        ),
+      ),
+    );
+  }
+}
+
+
 
 class _TreeEdgePainter extends CustomPainter {
   final List<_GraphNode> flatNodes;
@@ -168,21 +138,4 @@ class _TreeEdgePainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
-
-class _FlowchartNode extends StatelessWidget {
-  final String label;
-  final bool isSubworkflow;
-  const _FlowchartNode({required this.label, this.isSubworkflow = false});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      color: isSubworkflow ? Colors.orange[100] : Colors.blue[100],
-      child: Center(
-        child: Text(label, style: const TextStyle(fontWeight: FontWeight.bold)),
-      ),
-    );
-  }
 }
